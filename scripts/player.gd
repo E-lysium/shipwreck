29b6dca0b37cd74aebabc6be77774f774fb5484f
Maria Alexis Sales <mariaalexissales@gmail.com>
2024-08-10 18:50:56 -0700

    world creation and mvt scripts

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,19 +1,66 @@
 extends CharacterBody2D
 
-const max_speed = 400
-const accel = 1500
-const friction = 600
+const speed = 100
+var current_dir = "none"
 
-var input = Vector2.ZERO
+func _ready():
+	$AnimatedSprite2D.play("front_idle")
 
-func get_input():
-	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
-	input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
-	return input.normalized()
+func _physics_process(delta):
+	player_movement(delta)
 	
 func player_movement(delta):
-	input = get_input()
+	if Input.is_action_pressed("ui_right"):
+		current_dir = "right"
+		play_anim(1)
+		velocity.x = speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		current_dir = "left"
+		play_anim(1)
+		velocity.x = -speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_down"):
+		current_dir = "down"
+		play_anim(1)
+		velocity.x = 0
+		velocity.y = speed
+	elif Input.is_action_pressed("ui_up"):
+		current_dir = "up"
+		play_anim(1)
+		velocity.x = 0
+		velocity.y = -speed
+	else:
+		play_anim(0)
+		velocity.x = 0
+		velocity.y = 0
 	
-	if input === Vector2.ZERO:
-		if velocity.length() > (friction * delta)
-			velocity -= velocity.normalized()
+	move_and_slide()	
+
+func play_anim(movement):
+	var dir = current_dir
+	var anim = $AnimatedSprite2D
+	match dir:
+		"right":
+			anim.flip_h = false
+			if movement == 1:
+				anim.play("side_walk")
+			elif movement == 0:
+				anim.play("side_idle")
+		"left":
+			anim.flip_h = true
+			if movement == 1:
+				anim.play("side_walk")
+			elif movement == 0:
+				anim.play("side_idle")
+		"up":
+			if movement == 1:
+				anim.play("back_walk")
+			elif movement == 0:
+				anim.play("back_idle")
+		"down":
+			if movement == 1:
+				anim.play("front_walk")
+			elif movement == 0:
+				anim.play("front_idle")
+			
